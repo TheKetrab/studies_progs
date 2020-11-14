7a245856a47f7b560333d6a7a2839a14697d12b7
Bartłomiej Grochowski <43887439+TheKetrab@users.noreply.github.com>
2020-11-14 08:12:07 +0100

    Add files via upload

---
+++ WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSemApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CenterWindowOnScreen();
+        }
+
+        public void CenterWindowOnScreen()
+        {
+            double sw = SystemParameters.PrimaryScreenWidth;
+            double sh = SystemParameters.PrimaryScreenHeight;
+            double ww = this.Width;
+            double wh = this.Height;
+
+            this.Left = sw / 2 - ww / 2;
+            this.Top = sh / 2 - wh / 2;
+        }
+
+        private void RunProgressBar(object sender, RoutedEventArgs e)
+        {
+            /*
+            Task.Run(async () =>
+            {
+                for (pb.Percent = 0; pb.Percent < 100; pb.Percent++)
+                    await Task.Delay(50);
+            });
+            */
+
+            new Thread(() =>
+            {
+                for (pb.Percent = 0; pb.Percent < 100; pb.Percent++)
+                    Thread.Sleep(50);
+
+                Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    wpfText.FontSize = 40;
+                    wpfText.Text = "DONE";
+                }));
+
+            }).Start();
+
+
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void StackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ WPFApp/MyProgressBar.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSemApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MyProgressBar.xaml
+    /// </summary>
+    public partial class MyProgressBar : UserControl, INotifyPropertyChanged
+    {
+
+        private int _percent = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Percent
+        {
+            get { return _percent; }
+            set
+            {
+                if (value > 100) _percent = 100;
+                else if (value < 0) _percent = 0;
+                else _percent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public MyProgressBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
